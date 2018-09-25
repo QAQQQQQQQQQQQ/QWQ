c242d203ec96a08270fa1550a903da0146d99ca6
qaq <qaq@localhost.localdomain>
2018-09-25 05:34:04 -0400

    zuoye

---
+++ day01.py
@@ -0,0 +1,58 @@
+'''
+[1]
+print("enter a degree in celsius:")
+celsius = input(">>")
+a = 1.8 * celsius
+fahrenheit=a+32
+print("celsius is ")
+print(fahrenheit)
+
+
+
+
+[2]
+print("enter the radius and length of a cylinder:")
+radius = input(">>")
+length = input(">>")
+area = radius * radius * 3.14
+volume = area * length
+
+
+print("the area is " , area)
+print("the volume is", volume)
+
+[3]
+feet = input(">>")
+meters = feet * 0.305
+print(meters)
+ 
+
+[4]
+print("enter the amount of water in kilograms:")
+M=input(">>")
+finaltemerature=input(">>")
+initialtemerature=input(">>")
+
+a=finaltemerature - initialtemerature
+Q=M * a * 4184
+print(Q)
+
+
+[6]
+print("enter v0,v1,ant t:")
+v0 = input(">>")
+v1 = input(">>")
+t = input(">>")
+
+b=v1-v0
+a=b/t
+
+
+print(a)
+'''
+
+
+
+
+
+
